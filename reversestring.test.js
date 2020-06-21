bf5bd83d63605682a76a4012ae0094f3c67e9a6d
Fábio Daemon <contato@b2rocket.com.br>
2020-06-21 20:53:02 -0300

    teste

--- reversestring.test.js
+++
@@ -1,9 +0,0 @@
-const reverseString = require('./reversestring')
-
-test('reverseString function extists', () =>{
-    expect(reverseString).toBeDefined();
-});
-
-test('String reverses', () => {
-    expect(reverseString('HELLO')).toEqual('olleh');
-})
